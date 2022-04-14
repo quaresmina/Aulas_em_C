c97360bd9be90fbc122f61e4a72ea6a08eac6f4c
Ariel Quaresma <103616814+quaresmina@users.noreply.github.com>
2022-04-13 23:27:58 -0300

    Update Treinos.cpp
    apenas um teste

--- Treinos.cpp
+++ Treinos.cpp
@@ -3,6 +3,7 @@
 int main(){
 	char inicial;
 	int idade;
+	int ronaldo;
 	printf("Entre com sua idade e a sua inicial:\n");
 	scanf("%d %c", &idade, &inicial);
 	printf("Voce tem %d anos e seu nome comeca com %c\n", idade, inicial);
